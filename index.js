f81f3a2591503a59eeb091e18770406725513277
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-08-13 15:58:12 -0400

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const filesystem = require('./node_modules/graceful-fs/graceful-fs');
 const inquirer = require("inquirer");
 const {Circle, Square, Triangle} = require("./lib/shapes");
 // Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
